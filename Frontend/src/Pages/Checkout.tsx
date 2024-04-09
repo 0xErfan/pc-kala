c9920529b174560f756c95c83e7ff58a98ea34d8
Erfan <eftekharierfan5555555555@gmail.com>
2024-04-09 21:19:54 +0330

    update input validations

--- Frontend/src/Pages/Checkout.tsx
+++ Frontend/src/Pages/Checkout.tsx
@@ -20,21 +20,36 @@ const Checkout = () => {
 
                 <span className='md:pt-[160px] pt-[130px] block'></span>
 
-                <div className="container">
+                <div className="container bg-secondary-black rounded-md p-4">
 
                     <h3 className="text-white py-8">جزعیات صورت حساب</h3>
 
                     <div className="flex items-center gap-4">
-                        <div className="flex-1 grid grid-cols-2 gap-3 ch:mb-4">
-
-                            <Input fn={(name, value) => inputUpdater(name, value)} name="name" title="نام" />
-                            <Input fn={(name, value) => inputUpdater(name, value)} name="lName" title="نام خانوادگی" />
-                            {/* <Input fn={(name, value) => inputUpdater(name, value)} name="ostan" title="استان" /> */}
-                            {/* <Input fn={(name, value) => inputUpdater(name, value)} name="city" title="شهر" /> */}
-                            <Input fn={(name, value) => inputUpdater(name, value)} name="codePost" title="کد پستی (ده رقمی)" />
-                            <Input fn={(name, value) => inputUpdater(name, value)} name="phoneNum" title="شماره موبایل" type="number" placeHolder="09123456789" />
-                            <Input fn={(name, value) => inputUpdater(name, value)} name="email" title="پست الکترونیک (اختیاری)" required={false} type="email" placeHolder={"gmail.com@"} />
+                        <div className="flex-1">
+                            <div className="grid grid-cols-2 gap-3 ch:mb-4">
+                                <Input fn={inputUpdater} name="name" title="نام" />
+                                <Input fn={inputUpdater} name="lName" title="نام خانوادگی" />
+                                <Input fn={inputUpdater} name="ostan" title="استان">
+                                    <option disabled value={0}>شهر مورد نظر را انتخاب کنید</option>
+                                    <option value="tehe">تهران</option>
+                                    <option value="kh">خراسان رضوی</option>
+                                    <option value="esf">اصفهان</option>
+                                    <option value="shrz">شیراز</option>
+                                </Input>
+                                <Input fn={inputUpdater} name="province" title="خیابان" />
+                                <Input fn={inputUpdater} name="codePost" title="کد پستی (ده رقمی)" />
+                                <Input fn={inputUpdater} name="phoneNum" title="شماره موبایل" type="number" placeHolder="09123456789" />
+                                <Input fn={inputUpdater} name="email" title="پست الکترونیک (اختیاری)" required={false} type="email" placeHolder={"gmail.com@"} />
+                            </div>
+                            <div className="flex justify-center text-[13px] text-description-text mt-4 gap-4 flex-col">
+                                <p>توضیحات سفارش (اختیاری)</p>
+                                <textarea onChange={e => inputUpdater("orderDetails", e.target.value)} className="appearance w-full placeholder:text-[12px] h-[80px] max-h-[200px] bg-primary-black outline-none border rounded-md border-white/20 p-2" placeholder="یادداشت‌ها درباره سفارش شما، برای مثال نکات مهم درباره نحوه تحویل سفارش" cols={30} rows={10}></textarea>
+                            </div>
                         </div>
+
+
+
+
                         <div className="flex-1"></div>
                     </div>
                 </div>
--- Frontend/src/components/Input.tsx
+++ Frontend/src/components/Input.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from "react"
+import { ReactNode, useEffect, useState } from "react"
 
 interface inputProps {
     required?: boolean
@@ -6,17 +6,18 @@ interface inputProps {
     placeHolder?: string
     name: string
     type?: string
+    children?: ReactNode
     fn: (name: string, value: string) => unknown
 }
 
-export const Input = ({ required = true, title, placeHolder, type = "text", name, fn }: inputProps) => {
+export const Input = ({ required = true, title, placeHolder, type = "text", name, fn, children }: inputProps) => {
 
     const [value, setValue] = useState("")
     const [formError, setFormError] = useState<{ isErrorShown: boolean, errorMessage: string }>({ isErrorShown: false, errorMessage: "" })
 
     const showError = (status: boolean) => { setFormError(preve => ({ ...preve, isErrorShown: status })); return }
 
-    const inputValueChecker = (e: React.FocusEvent<HTMLInputElement, Element>) => {
+    const inputValueChecker = (e: React.FocusEvent<HTMLInputElement>) => {
 
         const valueToCheck = e.target.value.trim()
         if (valueToCheck.length == 0) showError(false)
@@ -26,6 +27,7 @@ export const Input = ({ required = true, title, placeHolder, type = "text", name
 
         switch (name) {
             case "name": { showError(value.length <= 2 || false); break }
+            case "province": { showError(!Boolean(value.length)); break }
             case "lName": { showError(value.length <= 2 || false); break }
             case "email": { showError(!emailRegex.test(value)); break }
             case "codePost": { showError(!(!isNaN(+value) && String(value).length == 10)); break }
@@ -40,6 +42,8 @@ export const Input = ({ required = true, title, placeHolder, type = "text", name
             case "email": { setFormError(preve => ({ ...preve, errorMessage: "ایمیل خود را به درستی وارد کنید" })); break }
             case "codePost": { setFormError(preve => ({ ...preve, errorMessage: "کد پستی را به درستی وارد کنید" })); break }
             case "phoneNum": { setFormError(preve => ({ ...preve, errorMessage: "شماره موبایل  نامعتبر است" })); break }
+            case "ostan": { setFormError(preve => ({ ...preve, errorMessage: "استان خود را انتخاب کنید" })); break }
+            case "province": { setFormError(preve => ({ ...preve, errorMessage: "اسم خیابان را وارد کنید" })); break }
             default: console.error("invalid input name => ", name)
         }
     }, [name])
@@ -49,16 +53,27 @@ export const Input = ({ required = true, title, placeHolder, type = "text", name
     return (
         <div className="flex flex-col gap-2 text-[12px] text-description-text">
             <label className="text-[11px]">{title}{required && " *"}</label>
-            <input
-                className={`appearance ltr bg-primary-black outline-none border rounded-md border-white/20 p-2`}
-                type={type}
-                placeholder={placeHolder}
-                value={value}
-                name={name}
-                onChange={e => setValue(e.target.value)}
-                onBlur={inputValueChecker}
-            />
-            {formError.isErrorShown && <span className={`text-white-red duration-200 text-[10px] ${formError.isErrorShown ? "opacity-100" : "opacity-0"} transition-all`}>{formError.errorMessage}</span>}
+            {
+                children
+                    ?
+                    <select
+                        className="bg-primary-black outline-none border rounded-md border-white/20 p-2" onChange={e => setValue(e.target.value)}>
+                        {children}
+                    </select>
+                    :
+                    <>
+                        <input
+                            className={`appearance bg-primary-black outline-none border rounded-md border-white/20 p-2`}
+                            type={type}
+                            placeholder={placeHolder}
+                            value={value}
+                            name={name}
+                            onChange={e => setValue(e.target.value)}
+                            onBlur={inputValueChecker}
+                        />
+                        {formError.isErrorShown && <span className={`text-white-red duration-200 text-[10px] ${formError.isErrorShown ? "opacity-100" : "opacity-0"} transition-all`}>{formError.errorMessage}</span>}
+                    </>
+            }
 
         </div>
     )
