4d00dfe1c30b521d3d759430a98f24a2b38a3abd
Erfan <eftekharierfan5555555555@gmail.com>
2024-05-04 16:59:47 +0330

    add middleware for login and register routes

---
+++ src/middleware.ts
@@ -0,0 +1,15 @@
+import { NextRequest, NextResponse } from "next/server";
+
+// Define the config object outside the middleware function
+export const config = {
+    matcher: ['/login', '/register'],
+};
+
+export function middleware(request: NextRequest) {
+    if (request.nextUrl.pathname == '/login' || request.nextUrl.pathname == '/register') {
+        let cookie = request.cookies.get('token');
+        console.log(cookie)
+        if (cookie) return NextResponse.redirect(new URL('http://localhost:3000', request.url));
+        return NextResponse.next();
+    }
+}
