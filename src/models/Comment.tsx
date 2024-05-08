249f4f2a05cbac98c40aa752c90c319e109dce39
Erfan <eftekharierfan5555555555@gmail.com>
2024-05-08 21:18:44 +0330

    add comment schema and model

---
+++ src/models/Comment.tsx
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const CommentSchema = new mongoose.Schema({
+    body: { type: String, required: true },
+    rate: { type: Number, default: 5, min: 0, max: 5 },
+    productID: { type: mongoose.Types.ObjectId, required: true, ref: 'Product' },
+    user: { type: mongoose.Types.ObjectId, required: true, ref: 'User' },
+    createdAt: { type: Date, default: Date.now(), immutable: true }
+})
+
+export const CommentModel = mongoose.models.Comment || mongoose.model('Comment', CommentSchema)
