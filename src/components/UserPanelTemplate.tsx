6b992cbdf25bb4e47bf5d96008cf3cf34790070c
Erfan <eftekharierfan5555555555@gmail.com>
2024-06-06 00:11:34 +0330

    update userPanelTemplate prop types

--- src/components/UserPanelTemplate.tsx
+++ src/components/UserPanelTemplate.tsx
@@ -1,18 +1,27 @@
 import { ReactNode } from "react"
 
-const UserPanelTemplate = ({ children, title }: { children: ReactNode, title: string }) => {
+interface UserPanelTmpProps {
+    children: ReactNode
+    title: string
+}
+
+const UserPanelTemplate = ({ children, title }: UserPanelTmpProps) => {
 
     return (
         <div className="flex-[3] relative">
+
             <div className="border border-dark-gold bg-secondary-black rounded-md">
+
                 <div className="inline-block space-y-2 font-peyda text-[15px] flex-col gap-2 pb-10 p-4">
                     <p>{title}</p>
                     <div className="w-5/6 ml-auto bg-white-red h-px rounded-sm"></div>
                 </div>
+
                 {children}
             </div>
+
         </div>
     )
 }
 
-export default UserPanelTemplate
+export default UserPanelTemplate;
