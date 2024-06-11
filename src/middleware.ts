c7fb38217f7ac097a47f6d2c73134a98347d004a
Erfan <eftekharierfan5555555555@gmail.com>
2024-06-11 13:18:20 +0330

    update middleware for auth routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,9 +8,30 @@ export default async function middleware(request: NextRequest) {
 
     const path = request.nextUrl.pathname
     let cookie = request.cookies.get('token')?.value;
- 
+
     if (path == '/login' || path == '/register') {
-        if (cookie) return NextResponse.redirect(new URL(`${process.env.NEXT_PUBLIC_BASE_PATH}/`, request.url));
+
+        if (!cookie) return NextResponse.next()
+
+        try {
+
+            const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_PATH}/auth/me`, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(cookie)
+            })
+
+            const data = await res.json()
+            console.log(data)
+
+            if (res.ok) return NextResponse.next()
+            request.cookies.delete('token')
+            return NextResponse.redirect(new URL(`${process.env.NEXT_PUBLIC_BASE_PATH}/`, request.url));
+
+        } catch (error) {
+            console.log(error)
+            if (cookie) return NextResponse.redirect(new URL(`${process.env.NEXT_PUBLIC_BASE_PATH}/`, request.url));
+        }
     }
 
     if (path == '/profile') {
@@ -22,9 +43,9 @@ export default async function middleware(request: NextRequest) {
     }
 
     // if (path == '/checkout') {
-        
+
     //     if (!cookie) return NextResponse.redirect(new URL(`${process.env.NEXT_PUBLIC_BASE_PATH}/`, request.url));
-        
+
     //     // this scope check for user basket so if its empty, checkout page is forbidden
     //     const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_PATH}/api/UserRelatedData/get`, {
     //         method: 'POST',
