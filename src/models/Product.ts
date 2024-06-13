e9642e31685c260e19f648d89b42a9f13c0f92ac
Erfan <eftekharierfan5555555555@gmail.com>
2024-06-13 13:33:04 +0330

    update product schema

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -4,7 +4,7 @@ const ProductSchema = new Schema({
     name: { type: String, required: true },
     price: { type: Number, required: true },
     discount: { type: Number, default: 0 },
-    image: { type: String, default: '' },
+    image: { type: [String] },
     type: {
         type: String,
         enum: ['pc', 'laptop', 'parts', 'accessory', 'console'],
