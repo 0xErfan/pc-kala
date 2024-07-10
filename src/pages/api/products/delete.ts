835a37c8de58070e1bb3f2c5833ac124f6d5e3d6
Erfan <eftekharierfan5555555555@gmail.com>
2024-07-10 20:58:56 +0330

    add delete product endpoint

---
+++ src/pages/api/products/delete.ts
@@ -0,0 +1,31 @@
+import connectToDB from "@/config/db";
+import ProductModel from "@/models/Product";
+import { authUser } from "@/utils";
+import { NextApiRequest, NextApiResponse } from "next";
+
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+
+    if (req.method !== 'POST') return res.status(421).json({ message: "This route can't be accessed without POST request_" })
+
+    try {
+
+        await connectToDB()
+
+        const user = await authUser({ isFromClient: false, cookie: req.cookies?.token })
+        if (user?.role !== 'ADMIN') return res.status(421).json({ message: 'This route is protected' })
+
+        const { _id } = req.body
+        if (!_id) return res.status(421).json({ message: 'need product id to delete it' })
+
+        await ProductModel.findOneAndDelete({ _id })
+
+        return res.json({ message: 'محصول با موفقیت خذف شد' })
+
+    } catch (err) {
+        console.log(err)
+        return res.status(421).json({ message: 'خطای ناشناخته / بعدا تلاش کنید', err })
+    }
+}
+
+export default handler;
